68dbfc173655b286a1d604363e1b9be5c520ed16
ouni <ouni@lipn.univ-paris13.fr>
2019-04-11 15:20:26 +0200

    parser

--- build/libraries/parser/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hiba/PMC-SOG/mc-sog/libraries/parser
-# Build directory: /home/hiba/PMC-SOG/mc-sog/build/libraries/parser
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
