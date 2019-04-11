3e2d389392f20d5cb3df727bad31f40198f58a5a
ouni <ouni@lipn.univ-paris13.fr>
2019-04-11 23:35:20 +0200

    mc-sog

--- build/libraries/parser/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hiba/PMC-SOG/mc-sog/libraries/parser/tests
-# Build directory: /home/hiba/PMC-SOG/mc-sog/build/libraries/parser/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(tests "/home/hiba/PMC-SOG/mc-sog/build/libraries/parser/tests/RdPtest")
-set_tests_properties(tests PROPERTIES  WORKING_DIRECTORY "/home/hiba/PMC-SOG/mc-sog/libraries/parser/examples")
