04acbf32fe916668f286b93ad8997adc238f7efa
abid <chiheb.abid@gmail.com>
2019-05-27 22:58:53 +0100

    modifié :         src/SogKripkeStateOTF.cpp 	modifié :         src/SogKripkeStateOTF.h

--- src/SogKripkeStateOTF.cpp
+++ src/SogKripkeStateOTF.cpp
@@ -8,23 +8,3 @@ SogKripkeStateOTF::~SogKripkeStateOTF()
     //dtor
 }
 
-SogKripkeStateOTF* SogKripkeStateOTF::clone() const
-{
-    return new SogKripkeStateOTF(*this);
-}
-
-size_t SogKripkeStateOTF::hash() const
-{
-    return m_state->getLDDValue();
-}
-
-int SogKripkeStateOTF::compare(const spot::state* other) const
-{
-    auto o = static_cast<const SogKripkeStateOTF*>(other);
-    size_t oh = o->hash();
-    size_t h = hash();
-    if (h < oh)
-        return -1;
-    else
-        return h > oh;
-}
--- src/SogKripkeStateOTF.h
+++ src/SogKripkeStateOTF.h
@@ -10,15 +10,30 @@ public:
     SogKripkeStateOTF(LDDState *st):m_state(st) {};
     virtual ~SogKripkeStateOTF();
 
-    SogKripkeStateOTF* clone() const override;
-    size_t hash() const override;
-
-    int compare(const spot::state* other) const override;
-
-    LDDState* getLDDState()
+    SogKripkeStateOTF* clone() const override
     {
+        return new SogKripkeStateOTF(m_state);
+    }
+    size_t hash() const override
+    {
+        return m_state->getLDDValue();
+    }
+
+    int compare(const spot::state* other) const override
+    {
+        auto o = static_cast<const SogKripkeStateOTF*>(other);
+        size_t oh = o->hash();
+        size_t h = hash();
+        if (h < oh)
+            return -1;
+        else
+            return h > oh;
+    }
+    LDDState* getLDDState() {
         return m_state;
     }
+protected:
+
 private:
     LDDState *m_state;
 };
